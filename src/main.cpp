1beca75d5e103cd285d6a96fee50b4ede5ef9937
neardeathbr <arcanexbr@gmail.com>
2020-08-19 18:52:36 +0300

    master

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+#ifndef STASSID
+#define STASSID "computer6"
+#define STAPSK  "12345678"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+const char* host = "api.weather.yandex.ru";
+const int httpsPort = 443;
+
+// Use web browser to view and copy
+// SHA1 fingerprint of the certificate
+const char fingerprint[] PROGMEM = "31 6D CA D6 6F C0 DC F5 E5 A4 DE 43 94 83 3A C2 AF BD 26 C0";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Use WiFiClientSecure class to create TLS connection
+  WiFiClientSecure client;
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  Serial.printf("Using fingerprint '%s'\n", fingerprint);
+  client.setFingerprint(fingerprint);
+
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  String url = "/v1/informers?lat=61.039726&lon=30.138224";
+  Serial.print("requesting URL: ");
+  Serial.println(url);
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "User-Agent: BuildFailureDetectorESP8266\r\n" +
+               "X-Yandex-API-Key: fb703d9e-180c-4d7f-9b77-c931a04e7345\r\n" +
+               "Connection: close\r\n\r\n");
+
+  Serial.println("request sent");
+  while (client.connected()) {
+    String line = client.readStringUntil('\n');
+    Serial.println(line);
+    
+    
+  }
+  ESP.restart();
+
+}
+
+void loop() {
+}
